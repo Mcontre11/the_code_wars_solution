639355cc232423fb436a6abccd9f0963226e030a
Resilient Coders <resilientcoders@admins-MacBook-Pro.local>
2021-11-04 21:48:49 -0400

    commit

---
+++ week9/divisibleByXandY/main.js
@@ -0,0 +1,12 @@
+// return if divisble by 2
+// compare n is divivisble  by x or y 
+// compare the that answer to 0
+// return is diisible is true and else if not 
+function isDivisible(n, x, y) {
+  if(n % x === 0 && n % y === 0){
+  return true
+}
+else{
+  return false
+}
+}
