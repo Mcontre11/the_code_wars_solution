00b17273a3cf6cf7a1a69b5fc8c18e2cb456af62
Resilient Coders <resilientcoders@admins-MacBook-Pro.local>
2021-11-09 21:21:57 -0500

    commit

---
+++ week10/abbreviateATwoWordName/main.js
@@ -0,0 +1,8 @@
+function abbrevName(name){
+  let firstLast = name.split(' ')
+  
+let firstInitial = (firstLast[0].charAt(0)+"."+firstLast[1].charAt(0)).toUpperCase()
+ 
+  return firstInitial
+   
+}
